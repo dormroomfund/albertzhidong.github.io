9db550e347426bd1d5af2c68680b164f4444b962
Albert Dong <albert.zhidong@gmail.com>
2017-09-27 19:25:00 -0400

    JS update

--- js/app.js
+++ js/app.js
@@ -4,14 +4,12 @@ $(document).ready(function() {
 
 
 function setHeight(){
-	alert("function called");
-	var container = document.getElementsByClassName('content-page');
-	for(i = 0; i < container.length; i++){
-		alert(typeof(container[i])); //object
-		if (container[i].children.find('.SingleGraphic').length > 0){
-			var text_height = container[i].find('.bodytext');
-			container[i].find('.SingleGraphic').attr('height') = text_height.attr('height')
-			alert('works');
-		}
+	// alert("function called");
+	var containerArray = document.getElementsByClassName('SingleGraphic');
+	for(i = 0; i < containerArray.length; i++){
+		// alert(typeof(containerArray[i])); //object
+		var text_height =containerArray[i].parent().find("bodytext");
+		containerArray[i].attr('height') = text_height.attr('height')
+		// alert('works');
 	}
-}
+};
