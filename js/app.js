22e57b6e8272a1529817d756722cb51aaa3e52d1
Albert Dong <albert.zhidong@gmail.com>
2017-09-27 19:02:46 -0400

    updated fonts

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 
 function setHeight(){
 	alert("function called");
-	var container = document.getElementsByClassName('.content-page');
+	var container = document.getElementsByClassName('content-page');
 	for(i = 0; i < container.length; i++){
 		alert(typeof(container[i])); //object
-		if (container[i].children('.SingleGraphic').length > 0){
+		if (container[i].children.find('.SingleGraphic').length > 0){
 			var text_height = container[i].find('.bodytext');
 			container[i].find('.SingleGraphic').attr('height') = text_height.attr('height')
 			alert('works');
