db37270b07c7870d06704c92f97c2d14dda8c9b8
Albert Dong <albert.zhidong@gmail.com>
2017-09-27 17:48:07 -0400

    hjbjh

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	setHeight();
+});
+
+
+function setHeight(){
+	alert("function called");
+	var container = document.getElementsByClassName('.content-page');
+	for(i = 0; i < container.length; i++){
+		alert(typeof(container[i])); //object
+		if (container[i].children('.SingleGraphic').length > 0){
+			var text_height = container[i].find('.bodytext');
+			container[i].find('.SingleGraphic').attr('height') = text_height.attr('height')
+			alert('works');
+		}
+	}
+}
