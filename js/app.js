a0950d896bc263ebe288966ef766a3d741bff6dd
Albert Dong <albert.zhidong@gmail.com>
2017-09-27 20:20:12 -0400

    update

--- js/app.js
+++ js/app.js
@@ -3,13 +3,40 @@ $(document).ready(function() {
 });
 
 
-function setHeight(){
-	// alert("function called");
-	var containerArray = document.getElementsByClassName('SingleGraphic');
-	for(i = 0; i < containerArray.length; i++){
-		// alert(typeof(containerArray[i])); //object
-		var text_height =containerArray[i].parent().find("bodytext");
-		containerArray[i].attr('height') = text_height.attr('height')
-		// alert('works');
-	}
-};
+// function setHeight(){
+// 	alert("function called");
+// 	var containerArray = document.getElementsByClassName('SingleGraphic');
+// 	for(i = 0; i < containerArray.length; i++){
+// 		alert(typeof(containerArray[i])); //object
+// 		var text_height =containerArray[i].parent().find("bodytext");
+// 		containerArray[i].attr('height') = text_height.attr('height')
+// 		alert('works');
+// 	}
+// };
+
+// function setHeight(){
+// 	alert("function called");
+// 	var containerArray = document.getElementsByClassName('content-page');
+// 	for(var i = 0; i < containerArray.length; i++){
+//   		var left = containerArray[i].getElementsByClassName('bodytext');
+//   		var right = containerArray[i].getElementsByClassName('SingleGraphic');
+//   	}
+
+
+
+
+// 	for(var i = 0; i < containerArray.length; i++){
+// 		alert(typeof(containerArray[i])); //object
+// 		var text_height = containerArray[i].parent().find("bodytext");
+// 		containerArray[i].attr('height') = text_height.attr('height')
+// 		alert('works');
+// 	}
+// };
+
+
+// var parent = document.getElementsByClassName('parent');
+// for(var i=0;i<parent.length;i++){
+//   var child = parent[i].getElementsByClassName('child');
+// for(var j=0;j<child.length;j++){
+//       child[j].style.color='red';
+//     }
